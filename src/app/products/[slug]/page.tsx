64a23187bdd969f6d28a0dc35c625ddf4b57a85e
seolleung2 <seolleung22@gmail.com>
2023-04-23 19:06:52 +0900

    fix: SSG 렌더링을 위한 getProducts, getProduct 함수 수정 및 적용 - 최상위경로에 data/products.json 파일 생성 - getProducts 함수 작성 : products.json 의 파일경로를 가지고 fs 모듈로 파일데이터를 읽어온 것을 파싱하여 리턴 - 데이터를 리턴하는데 적용되는 타입 정의 (Product) - slug 는 product 의 id 로 적용 - getProduct 함수 작성 : 전체 상품 데이터를 가져와서 파라미터로 들어온 상품 id 와 일치하는 상품 데이터를 리턴

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -9,31 +9,35 @@ type Props = {
 };
 
 // * 동적인 메타데이터 생성
-export const generateMetadata = ({ params }: Props) => {
+export const generateMetadata = async ({ params: { slug } }: Props) => {
+  const product = await getProduct(slug);
+
+  if (!product) return;
+
   return {
-    title: `제품의 이름: ${params.slug}`,
+    title: `제품의 이름: ${product.name}`,
   };
 };
 
-const PantsPage = ({ params: { slug } }: Props) => {
+const ProductPage = async ({ params: { slug } }: Props) => {
   // * 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  const product = getProduct(slug);
+  const product = await getProduct(slug);
 
   if (!product) notFound();
 
-  return <h1>{product} 설명 페이지</h1>;
+  return <h1>{product.name} 설명 페이지</h1>;
 };
 
-export default PantsPage;
+export default ProductPage;
 
 // * routing 의 종륲 : static, dynamic
 // 다이나믹 라우트 페이지 중에서 특정 경로의 페이지는 미리 만들어 두고 싶을 때 그 경로를 Next.js 에게 알려주도록 하는 함수
 
-export const generateStaticParams = () => {
+export const generateStaticParams = async () => {
   // * 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
-  const products = getProducts();
+  const products = await getProducts();
 
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 };
