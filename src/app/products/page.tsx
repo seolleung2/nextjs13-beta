6aff7309db3e501baa73d1cae22b7f02136deef1
seolleung2 <seolleung22@gmail.com>
2023-04-23 21:10:08 +0900

    fix: 고양이 문구를 CSR 컴포넌트로 분리 (fetch 를 사용한 CSR) - 고양이문구는 상품페이지에 영향을 주는 주요 데이터가 아닌 재미를 위해 가미한 데이터이다 - SSR 로 서버에 과부하를 줄 필요까진 없고 그렇다고 SSG 로 하자니 매번 같은 문구만 보게 하고 싶지 않다면 - 페이지에 있긴 하지만 중요하고 의미있는 컨텐츠가 아니라면, 나중에 렌더링되어도 상관없을 경우 CSR 방식으로 렌더링하도록 만들면 된다. - 해당 고양이문구 fetch 로직을 별도의 클라이언트 사이드 컴포넌트로 분리한다. (MeowArticle) - 이후 yarn build && yarn start 후 network tab 의 products 페이지를 보면 html 에 고양이 관련 사전 렌더링 요소가 빠져 있음을 확인할 수 있다.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,25 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import { getProducts } from "@/service/products";
-import styles from "./pages.module.css";
+import MeowArticle from "@/components/MeowArticle";
 
 const PRODUCTS_URL = "/products";
 
-// export const revalidate = 3;
-
 const ProductsPage = async () => {
   const products = await getProducts();
-
-  const res = await fetch("https://meowfacts.herokuapp.com/", {
-    next: {
-      revalidate: 3,
-    },
-    // cache: 'force-cache', // default cache, 지정해 주지 않으면 영원히 캐시가 되므로 SSG 처럼 동작
-    // cache: 'no-store', // SSR 처럼 동작
-  });
-  const { data } = await res.json();
-
-  const factText = data[0];
 
   return (
     <>
@@ -31,7 +18,7 @@ const ProductsPage = async () => {
           </li>
         ))}
       </ul>
-      <article className={styles.article}>{factText}</article>
+      <MeowArticle />
     </>
   );
 };
---
+++ src/components/MeowArticle.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import styles from "./MeowArticle.module.css";
+
+export default function MeowArticle() {
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    fetch("https://meowfacts.herokuapp.com/")
+      .then((res) => res.json())
+      .then((data) => setText(data.data[0]));
+  }, []);
+
+  return <article className={styles.article}>{text}</article>;
+}
