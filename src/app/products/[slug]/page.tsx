96646d2f2113e7f3e644b6d1b58a13ea5887bf9a
seolleung2 <seolleung22@gmail.com>
2023-04-23 18:08:31 +0900

    fix: 동적인 데이터로 구성하기 위한 리팩토링 (1) - 정적 변수를 사용하는 대신 (PRODUCTS) 서버 파일 에 있는 제품들의 리스트를 불러오도록 별도 경로 및 파일 생성 (src/service/products.ts) - 별도의 파일, 모듈, 함수로 만들어 코드의 재사용성을 높일 수 있음 - 소스코드의 구조, 디자인을 정하고 세부 리팩토링은 계속 진행될 예정 (getProduct 함수의 경우 하드코딩된 값을 리턴하기 때문) - yarn build 시 SSG 페이지가 늘어나 있음을 확인할 수 있다

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { notFound } from "next/navigation";
+import { getProduct, getProducts } from "@/service/products";
 
 type Props = {
   params: {
@@ -14,10 +15,13 @@ export const generateMetadata = ({ params }: Props) => {
   };
 };
 
-const PantsPage = ({ params }: Props) => {
-  if (params.slug === "nothing") notFound();
+const PantsPage = ({ params: { slug } }: Props) => {
+  // * 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+  const product = getProduct(slug);
 
-  return <h1>{params.slug} 설명 페이지</h1>;
+  if (!product) notFound();
+
+  return <h1>{product} 설명 페이지</h1>;
 };
 
 export default PantsPage;
@@ -26,7 +30,8 @@ export default PantsPage;
 // 다이나믹 라우트 페이지 중에서 특정 경로의 페이지는 미리 만들어 두고 싶을 때 그 경로를 Next.js 에게 알려주도록 하는 함수
 
 export const generateStaticParams = () => {
-  const products = ["pants", "skirts"];
+  // * 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+  const products = getProducts();
 
   return products.map((product) => ({
     slug: product,
---
+++ src/service/products.ts
@@ -0,0 +1,7 @@
+export const getProducts = () => {
+  return ["shirt", "pants", "skirt", "shoes", "dress"];
+};
+
+export const getProduct = (id: string) => {
+  return "shirt";
+};
