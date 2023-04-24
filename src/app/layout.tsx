1d1d633bb768351587d99f37aa8b84fec182264b
seolleung2 <seolleung22@gmail.com>
2023-04-24 13:56:15 +0900

    add: @next/font 설치 및 폰트 전역 설정 - yarn add @next/font 설치 - 관련 공식 문서 링크 : https://beta.nextjs.org/docs/optimizing/fonts - 최상위 layout 파일에서 폰트 불러와서 적용하기 (src/app/layout.tsx 참조) - 전역 혹은 부분 요소 별로 다른 폰트를 지정할 수 있음

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 import { Metadata } from "next";
 import "./globals.css";
 import style from "./layout.module.css";
+import { Open_Sans } from "@next/font/google";
+import { Nanum_Gothic } from "@next/font/google";
+
+const sans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 // * 모든 페이지에 공통으로 적용하기 위한 메타데이터 (공통 layout.tsx)
 export const metadata: Metadata = {
@@ -18,10 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       <body>
         <header className={style.header}>
-          <h1>Demo Note</h1>
+          <h1 className={gothic.className}>Demo Note</h1>
           <nav className={style.nav}>
             <Link href="/products">Products</Link>
             <Link href="/about">About</Link>
